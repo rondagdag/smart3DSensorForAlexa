02d86c3582327593075d08be3c443472174b0d61
rondagdag <rlyle78@gmail.com>
2017-12-30 11:34:34 -0600

    py

--- Walabot/walabot.py
+++ Walabot/walabot.py
@@ -1,47 +1,45 @@
 from __future__ import print_function
 from sys import platform
 from os import system
-import WalabotAPI as wlbt
+from imp import load_source
+WalabotAPI = load_source('WalabotAPI', 'C:/Program Files/Walabot/WalabotSDK/python/WalabotAPI.py')
 import socket, sys
 
 if __name__ == '__main__':
-	wlbt.Init()  # load the WalabotSDK to the Python wrapper	
-	wlbt.SetSettingsFolder()  # set the path to the essetial database files
-	wlbt.ConnectAny()  # establishes communication with the Walabot
-	wlbt.SetProfile(wlbt.PROF_SENSOR)  # set scan profile out of the possibilities
-	wlbt.SetThreshold(35)
-	wlbt.SetArenaR(50,400,4)
-	wlbt.SetArenaPhi(-45,45,2)
-	wlbt.SetArenaTheta(-20,20,10)
-	
-	wlbt.SetDynamicImageFilter(wlbt.FILTER_TYPE_MTI)  # specify filter to use
-	
-	wlbt.Start()  # starts Walabot in preparation for scanning
-	system('cls' if platform == 'win32' else 'clear')  # clear the terminal
-	numOfTargetsToDisplay = 1
-	if len(sys.argv) == 2:
-		TCP_IP = '127.0.0.1'
-		TCP_PORT = int(sys.argv[1])
-
-		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.bind((TCP_IP, TCP_PORT))
-		s.listen(1)
-		conn, addr = s.accept()			
-		while True:
-			wlbt.Trigger()  # initiates a scan and records signals
-			targets = wlbt.GetSensorTargets()  # provides a list of identified targets
-			finds = '{"targets": ['
-			index = 0
-			for i, t in enumerate(targets):
-				if i < numOfTargetsToDisplay:
-					index += 1
-					print('Target {}\nx = {}\ny = {}\nz = {}\n'.format(i+1, t.xPosCm, t.yPosCm, t.zPosCm))
-					finds += '{"x": "%s", "y": "%s", "z": "%s"}' % (t.xPosCm, t.yPosCm, t.zPosCm)
-					#if index < len(targets):
-					#	finds += ','												
-			finds += ']}'
-			my_str = 'TARGETS%s' % finds
-			conn.sendall(str.encode(my_str))			
-		conn.close()
-		wlbt.Stop()  # stops Walabot when finished scanning
-		wlbt.Disconnect()  # stops communication with Walabot
+    WalabotAPI.Init()  # load the WalabotSDK to the Python wrapper	
+    WalabotAPI.SetSettingsFolder()  # set the path to the essetial database files
+    WalabotAPI.ConnectAny()  # establishes communication with the Walabot
+    WalabotAPI.SetProfile(WalabotAPI.PROF_SENSOR)  # set scan profile out of the possibilities
+    WalabotAPI.SetThreshold(35)
+    WalabotAPI.SetArenaR(50,400,4)
+    WalabotAPI.SetArenaPhi(-45,45,2)
+    WalabotAPI.SetArenaTheta(-20,20,10)
+    WalabotAPI.SetDynamicImageFilter(WalabotAPI.FILTER_TYPE_MTI)  # specify filter to use
+    WalabotAPI.Start()  # starts Walabot in preparation for scanning
+    system('cls' if platform == 'win32' else 'clear')  # clear the terminal
+    numOfTargetsToDisplay = 1
+    if len(sys.argv) == 2:
+    	TCP_IP = '127.0.0.1'
+    	TCP_PORT = int(sys.argv[1])
+    	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    	s.bind((TCP_IP, TCP_PORT))
+    	s.listen(1)
+    	conn, addr = s.accept()			
+    	while True:
+    		WalabotAPI.Trigger()  # initiates a scan and records signals
+    		targets = WalabotAPI.GetSensorTargets()  # provides a list of identified targets
+    		finds = '{"targets": ['
+    		index = 0
+    		for i, t in enumerate(targets):
+    			if i < numOfTargetsToDisplay:
+    				index += 1
+    				print('Target {}\nx = {}\ny = {}\nz = {}\n'.format(i+1, t.xPosCm, t.yPosCm, t.zPosCm))
+    				finds += '{"x": "%s", "y": "%s", "z": "%s"}' % (t.xPosCm, t.yPosCm, t.zPosCm)
+    				#if index < len(targets):
+    				#	finds += ','												
+    		finds += ']}'
+    		my_str = 'TARGETS%s' % finds
+    		conn.sendall(str.encode(my_str))			
+    	conn.close()
+    	WalabotAPI.Stop()  # stops Walabot when finished scanning
+    	WalabotAPI.Disconnect()  # stops communication with Walabot
